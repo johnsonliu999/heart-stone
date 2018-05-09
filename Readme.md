18497ee93fd6eb60c78c0b1008cdd630a3df182b
glyn <liu.guo@husky.neu.edu>
2018-05-08 22:00:12 -0400

    firt commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# Heart Stone Fireplace
---
+++ src/main/java/glyn/fun/heart_stone/HeartStoneApplication.java
@@ -0,0 +1,12 @@
+package glyn.fun.heart_stone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartStoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartStoneApplication.class, args);
+	}
+}
